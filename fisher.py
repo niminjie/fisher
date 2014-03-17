bb8ceda17f594b81193d2c5394793c0b55ec05b2
niminjie <niminjiecide@gmail.com>
2014-03-17 15:31:53 +0800

    Modify inverse

--- fisher.py
+++ fisher.py
@@ -1,5 +1,6 @@
-import sys
+import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 class Fisher():
     def __init__(self, trn, trn_label, tst, tst_label):
@@ -30,38 +31,62 @@ class Fisher():
     def unitize_vector(self, v):
         return v / np.linalg.norm(v)
 
-    def cal_w(self):
+    def cal_threshold_1(self, w, avg_point_0, avg_point_1):
+        return np.dot(w.transpose(), avg_point_0 + avg_point_1) / 2
+
+    def cal_threshold_2(self, w, avg_point_0, avg_point_1):
+        return np.dot(w.transpose(), len(avg_point_1) * avg_point_0 + len(avg_point_0) * avg_point_1) / (len(avg_point_1) + len(avg_point_0))
+
+    def cal_threshold_3(self, w, avg_point_0, avg_point_1):
+        n0 = len(avg_point_0)
+        n1 = len(avg_point_1)
+
+    def cal_w(self, cal_thresh=cal_threshold_2):
         idx_0, idx_1 = self.separate(self.trn_label)
         point_0 = self.getter_idx(idx_0, self.trn)
         point_1 = self.getter_idx(idx_1, self.trn)
 
+        t_idx_0, t_idx_1 = self.separate(self.tst_label)
+        t_point_0 = self.getter_idx(t_idx_0, self.tst)
+        t_point_1 = self.getter_idx(t_idx_1, self.tst)
+
+        plt.xlabel(u'x')
+        plt.ylabel(u'y')
+        #plt.plot(point_0[:, 0], point_0[:, 1], 'ro')
+        #plt.plot(point_1[:, 0], point_1[:, 1], 'bo')
+        plt.plot(t_point_0[:, 0], t_point_0[:, 1], 'rx')
+        plt.plot(t_point_1[:, 0], t_point_1[:, 1], 'bx')
+
         avg_point_0 = self.avgPoint(point_0)
+        print 'avg_point_0 ', avg_point_0
         avg_point_1 = self.avgPoint(point_1)
+        print 'avg_point_1 ', avg_point_1
 
-        diff_0 = np.tile(avg_point_0, (len(idx_0), 1)) - point_0
-        diff_1 = np.tile(avg_point_1, (len(idx_1), 1)) - point_1
+        # diff_0 = np.tile(avg_point_0, (len(idx_0), 1)) - point_0
+        # diff_1 = np.tile(avg_point_1, (len(idx_1), 1)) - point_1
+        # print 'diff_0', diff_0
+        # print 'diff_1', diff_1
+        diff_0 = point_0 - np.tile(avg_point_0, (len(idx_0), 1))
+        diff_1 = point_1 - np.tile(avg_point_1, (len(idx_1), 1))
 
-        # s0 = len(idx_0) * (diff_0.transpose().dot(diff_0))
-        # s1 = len(idx_1) * (diff_1.transpose().dot(diff_1))
-        # sw = s0 + s1
-        # sw_ = sw ** -1
-        # w = sw_.dot(avg_point_0 - avg_point_1)
-        # thresh = ((avg_point_0 + avg_point_1) / 2).dot(w ** -1)
-        s0 = len(idx_0) * np.dot(diff_0.transpose(), diff_0)
-        s1 = len(idx_1) * np.dot(diff_1.transpose(), diff_1)
+        s0 = np.dot(diff_0.transpose(), diff_0)
+        s1 = np.dot(diff_1.transpose(), diff_1)
         sw = s0 + s1
-        sw_ = sw ** -1
+        #sw_ = sw ** -1
+        sw_ = np.linalg.inv(sw)
         w = np.dot(sw_, avg_point_0 - avg_point_1)
-        #thresh = np.dot(w.transpose(), avg_point_0 + avg_point_1) / 2
+        x = np.arange(-0.1, 0.2, 0.1)
+        y = w[1] / w[0] * x
+        plt.plot(x, y, 'g-', linewidth=2.0)
+        plt.show()
         n0 = len(point_0)
         n1 = len(point_1)
-        thresh = np.dot(w.transpose(), n1 * avg_point_0 + n0 * avg_point_1) / (n1 + n0)
-        #print thresh
+        thresh = cal_thresh(self, w, avg_point_0, avg_point_1)
         return w, thresh
 
-    # def train(self):
-    #     w, thresh = self.cal_w()
-    #     return w, thresh
+    def train(self):
+        w, thresh = self.cal_w()
+        return w, thresh
 
     def test(self):
         w, thresh = self.cal_w()
@@ -70,7 +95,7 @@ class Fisher():
         for i in range(0, len(self.tst)):
             y = w.transpose().dot(self.tst[i])
             # print y, thresh
-            if thresh <= y:
+            if thresh >= y:
                 result.append(1)
             else :
                 result.append(0)
@@ -78,7 +103,6 @@ class Fisher():
             if self.tst_label[i] == result[i]:
                 hits += 1
         print hits * 1.0 / len(self.tst_label)
-
 
 
 def convert_to_float(l):
@@ -103,6 +127,7 @@ def main():
     tst, tst_label = file_to_matrix(r'./synth/synth.te')
 
     fisher = Fisher(trn, trn_label, tst, tst_label)
+    fisher.train()
     fisher.test()
 
 if __name__ == '__main__':
